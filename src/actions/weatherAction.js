6af6a2a4036f79e3717638d5c929ebe06224ce14
alexiayoucef <ayoucef71@gmail.com>
2020-02-19 10:21:31 +0100

    feat(weather):optimization weather ation

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 // requete get sur le lien
 //axios pour la requete
 
+const baseUrl = "http://api.openweathermap.org/data/2.5";
+const appId = "&appid=fcf97c1aefda377b2f9c28ab6bbe4fee";
+
 export function getWeather(){
-    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=tenerife&appid=fcf97c1aefda377b2f9c28ab6bbe4fee");
+    return axios.get(baseUrl+"/weather?q=tenerife"+appId);
 }
